13beed3749f18350bc8c669e8a5548d8f480b59c
vpelss <vpelss@users.noreply.github.com>
2019-01-01 16:26:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -44,12 +44,27 @@ $c = substr($c,0,$length);
   <br> <br>
       <select name='length'>
         <option value="8">8</option>
-        <option value="12">12</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12"  selected>12</option>
+         <option value="13">8</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
         <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
         <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
         <option value="24">24</option>
-        <option value="28" selected>28</option>
-    </select>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+        <option value="28">28</option>   
+  </select>
   <br> <br>
 <input type='submit' value='Get Password' ">
 </form>
